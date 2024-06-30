23b0b08df1dfd585f38a62e0d3daf3b72752bb85
Ayush-Sharma410 <ayushsharma2267410@gmail.com>
2024-06-30 23:29:50 +0530

    customised exception.py and logger.py

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+from src.mlproject.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -9,16 +9,11 @@ def error_message_detail(error,error_detail:sys):
 
     return error_message
 
-    
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self,error_message,error_details:sys):
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
-    
+        self.error_message=error_message_detail(error_message,error_details)
+
     def __str__(self):
         return self.error_message
-    
-
-
-        
