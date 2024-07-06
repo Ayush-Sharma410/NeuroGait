64af06c5e35dcccda159359ec10245cf46c84e41
Ayush-Sharma410 <ayushsharma2267410@gmail.com>
2024-07-06 13:55:28 +0530

    deleted app.py and updated the path of datset from sql to csv

--- src/parkinsons_detection/components/data_ingestion.py
+++ src/parkinsons_detection/components/data_ingestion.py
@@ -21,10 +21,15 @@ class DataIngestion:
         self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
         try:
-            ##reading the data from mysql
-            df = read_sql_data()
-            logging.info("Reading completed mysql database")
+            # ##reading the data from mysql
+            # df = read_sql_data()
+            # logging.info("Reading completed mysql database")
+            
+        
+            df=pd.read_csv('notebook\Gait_Data___Arm_swing_02Apr2024.csv')
+            logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
